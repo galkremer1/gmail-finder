8b8f100284f25cb704de11a769c18538e1b7577e
galkremer1 <galkremer1@walla.co.il>
2015-06-05 13:14:26 +0300

    final changes

--- find_name.py
+++ find_name.py
@@ -1,6 +1,73 @@
 __author__ = 'galkremer'
+from datetime import datetime
+import os
 
-test = raw_input("Enter your name\n")
+from apiclient.discovery import build
+from httplib2 import Http
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
 
-for i in range(0, len(test)):
-    print test[i]
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gmail-quickstart.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatability with Python 2.6
+            credentials = tools.run(flow, store)
+        print 'Storing credentials to ' + credential_path
+    return credentials
+
+def main():
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
+    credentials = get_credentials()
+    service = build('gmail', 'v1', http=credentials.authorize(Http()))
+
+    search=raw_input("Search for:")
+
+    response = service.users().messages().list(userId='me', q=search).execute()
+    times = response['resultSizeEstimate']
+
+    if times==0:
+        print "The query you search for is not found in your gmail inbox"
+    elif times==1:
+        print "The query you search for was mentioned in 1 email in your gmail inbox."
+    else:
+        print "Your query was mentioned in %d emails in your gmail inbox." %times
+
+
+if __name__ == '__main__':
+    main()
